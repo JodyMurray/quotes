bc9fe1538519ef835469405ba13250a6ffbbd3ff
Jody M <jodyamm@hotmail.com>
2022-06-15 14:32:13 +0000

    Add quotes array into javascript

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var quotes = [
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        source: "Nelson Mandela"
+    },
+    {
+        quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+        source: "Benjamin Franklin"
+    },
+    {
+        quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.",
+        source: "Dr. Seuss"
+    },
+    {
+        quote: "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.",
+        source: "Maya Angelou"
+    },
+    {
+        quote: "The way I see it, if you want the rainbow, you gotta put up with the rain.",
+        source: "Dolly Parton"
+    },
+    {
+        quote: "There isn't a way things should be. There's just what happens, and what we do.",
+        source: "Terry Pratchett"
+    },
+    {
+        quote: "You never really learn much from hearing yourself speak.",
+        source: "George Clooney"
+    },
+    {
+        quote: "Life is a flower of which love is the honey.",
+        source: "Victor Hugo"
+    },
+    {
+        quote: "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+        source: "Mark Twain"
+    },
+    {
+        quote: "Every moment is a fresh beginning.",
+        source: "T.S. Eliot"
+    },
+    {
+        quote: "Don’t cry because it’s over, smile because it happened.",
+        source: "Dr. Seuss"
+    },
+    {
+        quote: "As my knowledge of things grew I felt more and more the delight of the world I was in.",
+        source: "Helen Keller"
+    },
+]
